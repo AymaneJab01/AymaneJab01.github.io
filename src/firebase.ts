aa8868165a053e7bc54bea9ff1137018f25bca94
AymaneJab01 <145920139+AymaneJab01@users.noreply.github.com>
2026-08-14 23:05:09 +0200

    fixing errors

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,15 +1,14 @@
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "YOUR_REAL_API_KEY",
-  authDomain: "YOUR_REAL_AUTH_DOMAIN",
-  projectId: "YOUR_REAL_PROJECT_ID",
-  storageBucket: "YOUR_REAL_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_REAL_MESSAGING_SENDER_ID",
-  appId: "YOUR_REAL_APP_ID",
+  apiKey: import.meta.env.FIREBASE_API_KEY,
+  authDomain: import.meta.env.PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.PUBLIC_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
-
 export const db = getFirestore(app);
